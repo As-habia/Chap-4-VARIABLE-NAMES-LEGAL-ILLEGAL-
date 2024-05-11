c319200af0ba8bead53e88ac69ee689ce7204b72
As-habia <afaanmehmil9>
2024-05-11 13:18:04 -0700

    first commit

---
+++ aap.js
@@ -0,0 +1,13 @@
+//task1
+// Task 1: Declare 3 variables in one statement
+var variable1, variable2, variable3;
+
+// Task 2: Legal variable names
+var $myVariable, _myVariable, myVariable1, my_variable, MYVARIABLE;
+
+// Task 3: Illegal variable names
+
+document.write("<p>Variable names can only contain letters, digits, underscores, and dollar signs. For example $my_1stVariable.</p>");
+document.write("<p>Variables must begin with a letter, underscore, or dollar sign. For example $name, _name, or name.</p>");
+document.write("<p>Variable names are case-sensitive.</p>");
+document.write("<p>Variable names should not be JS reserved keywords.</p>");
